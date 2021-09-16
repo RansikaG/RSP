963a1df5d4af4ae5303907cb884a79f8bd9560de
Ransika <ransika1998@gmail.com>
2021-09-16 13:30:37 +0530

    Update Simulation.m

--- Simulation.m
+++ Simulation.m
@@ -1,24 +1,28 @@
-L = 1000; %change this to 100000 for question 5
+L = 50; %change this to 100000 for question 5
 %generating a equiprobable binary sequence
 D = zeros(1,L); %generating a sequence of thousand zeros
 positions = randperm(L,L/2); %choosing 500 numbers randomly between 1 and 1000 without replacement
 D(positions) = ones(1,L/2); %replacing the zeros in D with ones in the randomly chosen places
+figure;
+stairs(1:L,D);
+axis([0 L -0.5 1.5])
+title("Received Signal");
 A = 1;
 %generating a sequence of pulses
-S = zeros(1,L); %generating a sequence of thousand zeros
-for i = 1:L
-if D(i) == 0 %assigning -A if D = 0
-S(i) = -1*A;
-else
-S(i) = A; %assigning A if D = 1
-end
-end
+S = -A*ones(1,L); %generating a sequence of thousand zeros
+S(D==1)= A;
+
+figure;
+stairs(1:L,S);
+axis([0 L -1.5*A 1.5*A])
+title("Received Signal");
 %generating AWGN with mean = 0 and variance = 1
-m = 0;
+mean = 0;
 sigma = 1;
-N = m + sigma*randn(1,L);
+N = mean + sigma*randn(1,L);
 %generating the received signal and plotting it
 R = S + N;
 figure;
-stairs([1:L],R);
+stairs(1:L,R);
+axis([0 L -1.5*A 1.5*A])
 title("Received Signal");
